3cefdc2d72c0cd977d31fc2ba356aaa5504a05c1
LeandroRocha <leandro.rocha@sof.to>
2024-06-18 17:32:04 -0300

    format_time

--- timer.sh
+++ timer.sh
@@ -4,20 +4,28 @@
 FILE=".timer_timestamp.txt"
 ACCU_FILE=".timer_accumulated.txt"
 
+# Função para formatar o tempo
+format_time() {
+    local total_seconds=$1
+    local hours=$(($total_seconds / 3600))
+    local minutes=$((($total_seconds % 3600) / 60))
+    local seconds=$(($total_seconds % 60))
+    echo "$hours hora(s), $minutes minuto(s) e $seconds segundo(s)"
+}
+
 # Função para iniciar ou continuar o timer
 start_timer() {
     if [ -f "$ACCU_FILE" ]; then
        accumulated_seconds=$(cat "$ACCU_FILE")
-       echo "Tempo total acumulado até agora: $(($accumulated_seconds / 3600)) hora(s) e $((($accumulated_seconds % 3600) / 60)) minuto(s)."
+       echo "Tempo total acumulado até agora: $(format_time $accumulated_seconds)"
        echo "Se desejar zerar o cronômetro, execute o comando 'stop'"
     else
        echo "0" > "$ACCU_FILE"
     fi
 
     date +%s > "$FILE"
-    echo "Cronômetro iniciado as $(date "+%H:%M:%S")"
+    echo "Cronômetro iniciado às $(date "+%H:%M:%S")"
 }
-
 
 # Função para pausar o timer e acumular o tempo
 _pause_timer() {
@@ -43,8 +51,8 @@ _pause_timer() {
 # Função para pausar o timer e acumular o tempo
 pause_timer() {
     _pause_timer
-    echo "Cronômetro pausado as $(date "+%H:%M:%S")"
-    echo "Tempo total acumulado até agora: $(($new_accumulated_seconds / 3600)) hora(s) e $((($new_accumulated_seconds % 3600) / 60)) minuto(s)."
+    echo "Cronômetro pausado às $(date "+%H:%M:%S")"
+    echo "Tempo total acumulado até agora: $(format_time $new_accumulated_seconds)"
     echo "Para retomar o tempo de onde parou, execute o comando 'start'"
 }
 
@@ -52,7 +60,7 @@ pause_timer() {
 check_timer() {
     if [ ! -f "$FILE" ] && [ -f "$ACCU_FILE" ];then
         total_seconds=$(cat "$ACCU_FILE")
-        echo "Tempo total acumulado até agora: $(($total_seconds / 3600)) hora(s) e $((($total_seconds % 3600) / 60)) minuto(s)."
+        echo "Tempo total acumulado até agora: $(format_time $total_seconds)"
         exit 1
     fi
 
@@ -62,25 +70,20 @@ check_timer() {
         current_time=$(date +%s)
         elapsed_seconds=$((current_time - start_time))
         total_seconds=$((accumulated_seconds + elapsed_seconds))
-        echo "Tempo total acumulado até agora: $(($total_seconds / 3600)) hora(s) e $((($total_seconds % 3600) / 60)) minuto(s)."
+        echo "Tempo total acumulado até agora: $(format_time $total_seconds)"
         exit 1
     else
         echo "Primeiro é necessário iniciar o cronômetro, usando o comando 'start'"
         exit 1
     fi
-
-
 }
 
 # Função para parar o timer e calcular o tempo total passado
 stop_timer() {
     _pause_timer  # Chama a função de pausar para atualizar o tempo acumulado
     accumulated_seconds=$(cat "$ACCU_FILE")
-    hours=$((accumulated_seconds / 3600))
-    minutes=$(((accumulated_seconds % 3600) / 60))
-
-    echo "Cronômetro parado as $(date "+%H:%M:%S")"
-    echo "Tempo total: $hours hora(s) e $minutes minuto(s)."
+    echo "Cronômetro parado às $(date "+%H:%M:%S")"
+    echo "Tempo total: $(format_time $accumulated_seconds)"
     rm "$ACCU_FILE"  # Limpa os arquivos para reiniciar o processo
 }
 
